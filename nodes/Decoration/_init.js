b68f71d24ac2b7b82525ac16cccef51393d90b94
StefansArya <stefansarya1@gmail.com>
2021-11-24 20:22:30 +0700

    Fix Decoration module URL that was not detected on Editor

---
+++ nodes/Decoration/_init.js
@@ -0,0 +1,18 @@
+// We don't have graphics node for non-browser :3
+// Let's just use this file to load .sf.mjs and .sf.css
+
+let Blackprint = window.Blackprint.loadScope({
+	// You can find the URL on Blackprint menu -> Modules
+	// This will also be exported to JSON if this module's nodes is being used
+	url: import.meta.url,
+
+	// This will autoload (*.sf.mjs) and (*.sf.css) file for Browser
+	hasInterface: true,
+});
+
+// Global shared context
+let Context = Blackprint.getContext('Decoration');
+
+// This is needed to avoid duplicated event listener when using hot reload
+// Event listener that registered with same slot will be replaced
+Context.EventSlot = {slot: 'my-private-event-slot'};
