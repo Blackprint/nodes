157eb0d5619b54405583b608cb9ee50e038505ae
StefansArya <stefansarya1@gmail.com>
2022-02-08 12:26:37 +0700

    Add nodes that can trigger when it receive new any data

---
+++ nodes/Data/Any/To/Trigger.js
@@ -0,0 +1,20 @@
+Blackprint.registerNode("Data/Any/To/Trigger",
+class EmptyNode extends Blackprint.Node {
+	static input = { Value: Any };
+	static output = { Call: Function };
+
+	constructor(instance){
+		super(instance);
+
+		let iface = this.setInterface('BPIC/Data/Minimal');
+		iface.title = "Trigger on any data changes";
+		iface.element = document.createTextNode('Fx()');
+	}
+
+	update(){
+		this.ref.Output.Call();
+	}
+});
+
+// Register with default interface
+Blackprint.registerInterface("BPIC/Data/Minimal", class extends Blackprint.Interface {});
