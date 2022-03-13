fdd53a32efc27f2bc17a8e4dd4b0d5f057f8a772
StefansArya <stefansarya1@gmail.com>
2022-03-13 08:34:30 +0700

    Add node for obtaining data from object properties

---
+++ nodes/Data/Object/GetValue.js
@@ -0,0 +1,95 @@
+Blackprint.registerNode('Data/Object/GetValue',
+class extends Blackprint.Node {
+	static input = { Object };
+	static output = { Value: null };
+
+	constructor(instance){
+		super(instance);
+
+		let iface = this.setInterface('BPIC/Data/Object/GetValue');
+		iface.title = "Get Object Value";
+		iface.description = 'Dive through object properties';
+	}
+
+	init(){
+		let { Output } = this.ref;
+
+		// Let's remove data after the cable was disconnected
+		this.iface.on('cable.disconnect', Context.EventSlot, () => {
+			Output.Value = null;
+		});
+	}
+
+	update(){
+		let { Input, Output } = this.ref;
+		let val = null;
+
+		try{
+			if(this._getter == null) return;
+			val = this._getter(Input.Object);
+		} catch(e) {
+			if(e.message.includes === "Cannot read properties of"){
+				Output.Value = null;
+				return;
+			}
+
+			this.iface._toast.error(e.message);
+		}
+
+		Output.Value = val;
+		this.iface._toast.clear();
+	}
+
+	imported(data){ Object.assign(this.iface.data, data) }
+
+	recreateFunc(props){
+		if(props === '')
+			return this._getter = null;
+
+		// ToDo: support symbol inside of quotes => props["my-props"]
+		// Symbol outside of quotes is forbidden => props.myStuff().hello?.world
+		// Currently this node will support => props.myStuff["hello"].world
+		if(/[`~!@#$%^&*()\-=+{}|\\;:,<>/?]/.test(props))
+			return this.iface._toast.error("Can't use symbol when destructuring properties");
+
+		if(props.slice(0, 1) === '[')
+			props = `_a_${props}`;
+		else props = `_a_.${props}`;
+
+		this.iface._toast.clear();
+
+		try{
+			this._getter = Function('_a_', 'return '+props);
+		} catch(e) {
+			if(e.message.includes('Unexpected token')){
+				this.iface._toast.warn('...?');
+				return;
+			}
+		}
+
+		this.update();
+	}
+
+	// Remote sync in
+	syncIn(id, data){
+		if(id === 'props') this.iface.props = data;
+	}
+});
+
+Blackprint.registerInterface('BPIC/Data/Object/GetValue',
+Context.IFace.GetValue = class extends Blackprint.Interface {
+	constructor(node){
+		super(node);
+		this._toast = new NodeToast(this);
+
+		this.data = {
+			_props: '',
+			get props(){ return this._props },
+			set props(val){
+				this._props = val;
+				node.recreateFunc(val);
+				node.syncOut('props', val);
+			},
+		};
+	}
+});
