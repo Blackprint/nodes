03af80831e211051d192a568f3bd914cbc2a3a52
StefansArya <stefansarya1@gmail.com>
2022-03-13 08:33:44 +0700

    Add `NodeToast`

--- nodes/Data/_init.js
+++ nodes/Data/_init.js
@@ -22,3 +22,78 @@ Context.EventSlot = {slot: 'my-private-event-slot'};
 let $ = globalThis.sf?.$;
 if(!Blackprint.Environment.isBrowser)
 	$ = ()=> [];
+
+
+// Bootstrap for add toast on node decoration
+let NodeToast = Context.NodeToast = class NodeToast {
+	constructor(iface){
+		this.iface = iface;
+		this._have = false;
+	}
+
+	clear(){
+		// Early return to improve performance
+		if(this._have === false) return;
+
+		if(this.haveInfo)
+			this.haveInfo.destroy();
+		if(this.haveWarn)
+			this.haveWarn.destroy();
+		if(this.haveError)
+			this.haveError.destroy();
+
+		this.haveInfo = false;
+		this.haveWarn = false;
+		this.haveError = false;
+	}
+
+	_reduceText(text){
+		this._have = true;
+		return text.replace(/\w{15,}/g, full => full.slice(0, 5)+'...');
+	}
+
+	info(text){
+		if(!this.iface.$decoration) return;
+		let temp = text;
+		text = this._reduceText(text);
+
+		if(this.haveInfo)
+			this.haveInfo.text = text;
+		else
+			this.haveInfo = this.iface.$decoration.info(text);
+
+		this.haveInfo._raw = temp;
+	}
+
+	warn(text){
+		if(!this.iface.$decoration) return;
+		let temp = text;
+		text = this._reduceText(text);
+
+		if(this.haveWarn)
+			this.haveWarn.text = text;
+		else
+			this.haveWarn = this.iface.$decoration.warn(text);
+
+		this.haveWarn._raw = temp;
+	}
+
+	error(text){
+		if(!this.iface.$decoration) return;
+		let temp = text;
+		text = this._reduceText(text);
+
+		if(this.haveError)
+			this.haveError.text = text;
+		else
+			this.haveError = this.iface.$decoration.error(text);
+
+		this.haveError._raw = temp;
+	}
+
+	success(text){
+		if(!this.iface.$decoration) return;
+		let ref = this.iface.$decoration.success(this._reduceText(text));
+		ref._raw = text;
+	}
+}
