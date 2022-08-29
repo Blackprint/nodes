87f6333889c5d3102500b81481618a808364c331
StefansArya <stefansarya1@gmail.com>
2022-08-29 16:39:19 +0700

    Add `Data/Boolean/To/Route` node

--- nodes/Data/Boolean/To/Route.js
+++ nodes/Data/Boolean/To/Route.js
@@ -1,26 +1,26 @@
 /**
- * Trigger a function call if the input receive `true` value
+ * Route to other node if the input receive `true` value
  * @blackprint node
  */
-Blackprint.registerNode("Data/Boolean/To/Trigger",
+Blackprint.registerNode("Data/Boolean/To/Route",
 class extends Blackprint.Node {
 	static input = { Value: Boolean };
-	static output = { Call: Function };
+	static output = { Route: Blackprint.Port.Route };
 
 	constructor(instance){
 		super(instance);
 
 		let iface = this.setInterface('BPIC/Data/Minimal');
-		iface.title = "Trigger on true";
+		iface.title = "Route on true";
 		iface.data = {signal: true};
 	}
 
 	createIcon(){
-		return document.createTextNode('Call()');
+		return document.createTextNode('Route ➤');
 	}
 
 	update(){
 		if(this.ref.Input.Value === this.iface.data.signal)
-			this.ref.Output.Call();
+			this.ref.Output.Route();
 	}
 });
