0c3698c0b1e4089f3264aa14cd3bf0b7822843b2
StefansArya <stefansarya1@gmail.com>
2022-10-30 12:06:13 +0700

    Add node to convert function port to route

---
+++ nodes/Data/Route/From/Callback.js
@@ -0,0 +1,20 @@
+/**
+ * Route to other node if the input receive `true` value
+ * @blackprint node
+ */
+Blackprint.registerNode("Data/Route/From/Callback",
+class extends Blackprint.Node {
+	static input = { Value: Blackprint.Port.Trigger(port => port.iface.ref.Output.Route()) };
+	static output = { Route: Blackprint.Port.Route };
+
+	constructor(instance){
+		super(instance);
+
+		let iface = this.setInterface('BPIC/Data/Minimal');
+		iface.title = "Route on Call";
+	}
+
+	createIcon(){
+		return document.createTextNode('Route ➤');
+	}
+});
